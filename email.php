8a43d3cf3080f1bcb90a20d8cede0359d59b5bb9
Jonathan Veit <jonathan.veit@gmail.com>
2015-01-21 22:41:19 -0500

    Inlined CSS for speed optimization

--- email.php
+++ email.php
@@ -2,7 +2,7 @@
 
 require './scripts/PHPMailerAutoload.php';
 
-$name = $_POST['fullname'];
+$name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
@@ -10,13 +10,13 @@ $message .= "\n\nSent Through JonVeit.com";
 
 $mail = new PHPMailer;
 
-$mail->SMTPDebug = 3;                               // Enable verbose debug output
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.mandrillapp.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'jonathan.veit@gmail.com';                 // SMTP username
-$mail->Password = 'asd90123';	// SMTP password
+$mail->Password = 'Rpe9t-oiBPTbv7JA6fLBIg';	// SMTP password
 $mail->SMTPSecure = 'tls';	// Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;	// TCP port to connect to
 
@@ -29,19 +29,14 @@ $mail->Body    = $message;
 
 
 if(!$mail->send()) {
-    $output = 'Message could not be sent.';
+    $output = 'Message could not be sent. ';
     $output .= 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
     $output = 'Message has been sent';
 }
 
+echo $output;
 
 ?>
 
-<div id="content">
-	
-	<p> <?php echo $output; ?> </p>
 
-</div>
-
-	
